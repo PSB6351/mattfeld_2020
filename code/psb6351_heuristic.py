fb5dfc53e40a97997ca0d32f3288d3bda82c4f36
Benjelene <70165790+Benjelene@users.noreply.github.com>
2020-09-23 17:05:39 -0400

    PSB_6351_Heuristic file updated
    Updating heuristic file completed based on heudiconv presentation in order to organize DICOM data into BIDS format.

--- code/psb6351_heuristic.py
+++ code/psb6351_heuristic.py
@@ -18,36 +18,25 @@ def infotodict(seqinfo):
     subindex: sub index within group
     """
 
-    data = create_key('run{item:03d}')
-    info = {data: []}
+    t1w = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/anat/sub-{subject}_T1w')
+    dwi = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/dwi/sub-{subject}_dwi')
+    func = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/func/sub-{subject}_task-REVL_rec-{rec}_run-{item:01d}_bold')
+    info = {t1w: [], dwi: [], func: []}
     last_run = len(seqinfo)
 
-    for s in seqinfo:
-        """
-        The namedtuple `s` contains the following fields:
-
-        * total_files_till_now
-        * example_dcm_file
-        * series_id
-        * dcm_dir_name
-        * unspecified2
-        * unspecified3
-        * dim1
-        * dim2
-        * dim3
-        * dim4
-        * TR
-        * TE
-        * protocol_name
-        * is_motion_corrected
-        * is_derived
-        * patient_id
-        * study_description
-        * referring_physician_name
-        * series_description
-        * image_type
-        """
-
-        info[data].append(s.series_id)
+    for idx, s in enumerate (seqinfo):
+        if (s.dim1 == 256) and (s.dim2 == 256) and (s.dim3 == 176) and (s.dim4 ==1) and ('T1w' in s.protocol_name):
+            info[t1w] = [s.series_id]
+        if (s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (s.dim4 ==103) and ('dMRI' in s.protocol_name):
+            info[dwi] = [s.series_id]
+        if (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 ==355) and ('fMRI' in s.protocol_name):
+            if ('_Study_1' in s.protocol_name):
+                info[func].append({'item':s.series_id, 'rec':'Study_1'})
+            if ('_Study_2' in s.protocol_name):
+                info[func].append({'item':s.series_id, 'rec':'Study_2'})
+            if ('_Study_3' in s.protocol_name):
+                info[func].append({'item':s.series_id, 'rec':'Study_3'})
+            else
+                info[func].append({'item':s.series_id, 'rec':'Study_4'})
     return info
 
