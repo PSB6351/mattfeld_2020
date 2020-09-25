c640857115d5619bf011bda3fcec5c5906c70749
Benjelene <70165790+Benjelene@users.noreply.github.com>
2020-09-25 10:38:52 -0400

    Update psb6351_heuristic.py

--- code/psb6351_heuristic.py
+++ code/psb6351_heuristic.py
@@ -20,23 +20,41 @@ def infotodict(seqinfo):
 
     t1w = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/anat/sub-{subject}_T1w')
     dwi = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/dwi/sub-{subject}_dwi')
-    func = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/func/sub-{subject}_task-REVL_rec-{rec}_run-{item:01d}_bold')
-    info = {t1w: [], dwi: [], func: []}
+    func = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/func/sub-{subject}_task-REVL_acq-{rec}_bold')
+    func_fmap = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/fmap/sub-{subject}_acq-func_dir-{dir}_run-{item}_epi')
+    dwi_fmap = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/fmap/sub-{subject}_acq-dwi_dir-{dir}_run-{item}_epi')
+    loc1 = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/func/sub-{subject}_task-loc_run-1_bold')
+    loc2 = create_key('C:/Users/benji/OneDrive/Documentos/mattfeld_2020/dset/sub-{subject}/func/sub-{subject}_task-loc_run-2_bold')
+    info = {t1w: [], dwi: [], func: [], func_fmap: [], dwi_fmap: [], loc1: [], loc2: []}
     last_run = len(seqinfo)
 
     for idx, s in enumerate (seqinfo):
         if (s.dim1 == 256) and (s.dim2 == 256) and (s.dim3 == 176) and (s.dim4 ==1) and ('T1w' in s.protocol_name):
             info[t1w] = [s.series_id]
-        if (s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (s.dim4 ==103) and ('dMRI' in s.protocol_name):
+        elif (s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (s.dim4 ==103) and ('dMRI' in s.protocol_name):
             info[dwi] = [s.series_id]
-        if (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 ==355) and ('fMRI' in s.protocol_name):
+        elif (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 ==355) and ('fMRI' in s.protocol_name):
             if ('_Study_1' in s.protocol_name):
-                info[func].append({'item':s.series_id, 'rec':'Study_1'})
-            if ('_Study_2' in s.protocol_name):
-                info[func].append({'item':s.series_id, 'rec':'Study_2'})
-            if ('_Study_3' in s.protocol_name):
-                info[func].append({'item':s.series_id, 'rec':'Study_3'})
+                info[func].append({'rec':'Study_1'})
+            elif ('_Study_2' in s.protocol_name):
+                info[func].append({'rec':'Study_2'})
+            elif ('_Study_3' in s.protocol_name):
+                info[func].append({'rec':'Study_3'})
             else
-                info[func].append({'item':s.series_id, 'rec':'Study_4'})
+                info[func].append({'rec':'Study_4'})
+        elif (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 == 1) and ('DistortionMap' in s.protocol_name):
+            if ('fMRI' in s.protocol_name) and ('PA' in s.protocol_name):
+                info[func_fmap].append({'rec': 'func_fmap', 'dir': 'PA'})
+            else ('fMRI' in s.protocol_name) and ('AP' in s.protocol_name):
+                info[func_fmap].append({'rec': 'func_fmap', 'dir': 'AP'})
+        elif (s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (s.dim4 == 1) and ('DistortionMap' in s.protocol_name):
+            if ('dMRI' in s.protocol_name) and ('AP' in s.protocol_name):
+                info[dwi_fmap].append({'rec': 'dwi_fmap', 'dir': 'PA'})
+            else ('dMRI' in s.protocol_name) and ('PA' in s.protocol_name):
+                info[dwi_fmap].append({'rec': 'dwi_fmap', 'dir': 'PA'})        
+        elif (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 ==304) and ('ROI_loc_1' in s.protocol_name):
+            info[loc1] = [s.series_id]
+        elif (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 ==304) and ('ROI_loc_2' in s.protocol_name):
+            info[loc2] = [s.series_id]
     return info
 
