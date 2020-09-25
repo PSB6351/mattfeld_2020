faf468400ca5ebef991b75bce09a5c16a3a78b42
nbrij001 <nbrij001@fiu.edu>
2020-09-25 10:50:16 -0400

    Update psb6351_heuristic.py
    Added localization task and changed 'if' to 'elif'

--- code/psb6351_heuristic.py
+++ code/psb6351_heuristic.py
@@ -22,32 +22,40 @@ def infotodict(seqinfo):
     dwi = create_key('/Users/nbrijmoh/Documents/mattfeld_2020/dset/sub-{subject}/dwi/sub-{subject}_dwi')
     func = create_key('/Users/nbrijmoh/Documents/mattfeld_2020/dset/sub-{subject}/func/sub-{subject}_task-REVL_rec-{rec}_run-(item:01d)_bold')
     fimap = create_key('/Users/nbrijmoh/Documents/mattfeld_2020/dset/sub-{subject}/fimap/sub-{subject}_acq-{rec}_dir-{dir}')
+    local = create_key('/Users/nbrijmoh/Documents/mattfeld_2020/dset/sub-{subject}/local/sub-{subject}_task_bold')
     
-    info = {tw: [], dwi: [], func: [], fimap: []}
+    info = {tw: [], dwi: [], func: [], fimap: [], local: []}
     last_run = len(seqinfo)
 
     for idx, s in enumerate(seqinfo):
-        if (s.dim1 == 256) and (s.dim2 == 256) and (s.dim3 == 176) and (sim.4 == 1) and ('T1w' in s.protocol_name):
+        if (s.dim1 == 256) and (s.dim2 == 256) and (s.dim3 == 176) and (s.dim4 == 1) and ('T1w' in s.protocol_name):
             info[t1w] = [s.series_id]
-        if ((s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (sim.4 == 103) and ('dMRI' in s.protocol_name):
+        elif ((s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (s.dim4 == 103) and ('dMRI' in s.protocol_name):
             info[dwi] = [s.series_id]
-        if (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (sim.4 == 355) and ('fMRI' in s.protocol_name):
-            if ('_Study_1' in s.protocol_name):
+        elif (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 == 355) and ('fMRI' in s.protocol_name):
+            elif ('_Study_1' in s.protocol_name):
                 info[func].append({'item': s.series_id, 'rec': 'Study_1'})
-            if ('_Study_2' in s.protocol_name):
+            elif ('_Study_2' in s.protocol_name):
                 info[func].append({'item': s.series_id, 'rec': 'Study_2'})
-            if ('_Study_3' in s.protocol_name):
+            elif ('_Study_3' in s.protocol_name):
                 info[func].append({'item': s.series_id, 'rec': 'Study_3'})
-            else
+            elif
                 info[func].append({'item': s.series_id, 'rec': 'Study_4'})
-        if (s.dim4 == 4) and ('DistortionMap' in s.protocol_name):
-            if ('fMRI' in s.protocol_name) and ('PA' in s.protocol_name):
+        elif (s.dim4 == 4) and ('DistortionMap' in s.protocol_name):
+            elif ('fMRI' in s.protocol_name) and ('PA' in s.protocol_name):
                 info[fimap].append({'rec': 'func', 'dir': 'PA'})
-            if ('fMRI' in s.protocol_name) and ('AP' in s.protocol_name):
+            elif ('fMRI' in s.protocol_name) and ('AP' in s.protocol_name):
                 info[fimap].append({'rec': 'func', 'dir': 'AP'})
-            if ('dMRI' in s.protocol_name) and ('PA' in s.protocol_name):
+            elif ('dMRI' in s.protocol_name) and ('PA' in s.protocol_name):
                 info[fimap].append({'rec': 'func', 'dir': 'PA'})
-            if ('dMRI' in s.protocol_name) and ('AP' in s.protocol_name):
+            elif ('dMRI' in s.protocol_name) and ('AP' in s.protocol_name):
                 info[fimap].append({'rec': 'func', 'dir': 'AP'})
+         elif (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66)and (s.dim4 == 304) and ('ROI' in s.protocol_name): 
+            elif ('loc_1' in s.protocol_name):
+                info[local].append({'task': s.series_id, 'rec': 'loc_1'})
+            elif ('loc_2' in s.protocol_name):
+                info[local].append({'task': s.series_id, 'rec': 'loc_2'})
+            else:
+                 pass
     return info
 
